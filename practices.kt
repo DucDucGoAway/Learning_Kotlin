c92c487c7261c14ed3c9c6481cf8405060d6a01f
Duc <92260454+DucDucGoAway@users.noreply.github.com>
2023-01-19 14:01:55 +0100

    made temperature converter work
    19012022

--- practices.kt
+++ practices.kt
@@ -1,23 +1,23 @@
 //notification Summary
 /*
 fun main() {
-   	val morningNotification = 51
-   	val eveningNotification = 135
+    val morningNotification = 51
+    val eveningNotification = 135
     
     printNotificationSummary(morningNotification)
     printNotificationSummary(eveningNotification)
 }
-
+​
 fun printNotificationSummary(numberOfMessages: Int) {
     if(numberOfMessages <= 99) {
-    	println("You have $numberOfMessages notifications")
+        println("You have $numberOfMessages notifications")
     }
     else {
         println("You have 99+ notifications")
     }
 }
 */
-
+​
 //movie Ticket Price
 /*
 fun main() {
@@ -31,41 +31,58 @@ fun main() {
     println("The movie ticket price for a person aged $adult is ${ticketPrice(adult, isMonday)}.")
     println("The movie ticket price for a person aged $senior is ${ticketPrice(senior, isMonday)}.")
 }
-
+​
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
     return when(age) {
-        in 0..12			-> 15
-        in 13..60			-> if(isMonday == true) {25} else {30}
-        in 61..100			-> 20
-        else				-> -1
+        in 0..12            -> 15
+        in 13..60           -> if(isMonday == true) {25} else {30}
+        in 61..100          -> 20
+        else                -> -1
     }
 }
 */
-
+​
 // Temperature Converter
-
+// the tutorials made by Google straight up gave me the wrong formulas
+// also their code was kinda weird to understand so I made my own
+​
 fun main() {
+    val initialUnit: String = "Celcius"
+    val finalUnit: String = "Fahrenheit"
+    val initialMeasurement: Double = 30.0
     
+    println("$initialMeasurement degrees $initialUnit are ${finalMeasurement(initialUnit, finalUnit, initialMeasurement)} degrees $finalUnit")
 }
-
-
-fun printFinalTemperature(
-    initialMeasurement: Double, 
-    initialUnit: String, 
-    finalUnit: String, 
-    conversionFormula: (Double) -> Double
-) {
-    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
-    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
+​
+fun finalMeasurement(initialUnit: String, finalUnit: String, initialMeasurement: Double): Double {
+    return when(initialUnit) {
+        "Celcius"       -> when(finalUnit) {
+            "Celcius"       -> initialMeasurement
+            "Fahrenheit"    -> 1.8 * initialMeasurement + 32
+            "Kelvin"        -> initialMeasurement + 273.15
+            else            -> 0.0
+        }
+        "Fahrenheit"    -> when(finalUnit) {
+            "Celcius"       -> 1.8 * (initialMeasurement - 32)
+            "Fahrenheit"    -> initialMeasurement
+            "Kelvin"        -> 1.8 * (initialMeasurement - 32) + 273.15
+            else            -> 0.0
+        }
+        "Kelvin"        -> when(finalUnit) {
+            "Celcius"       -> initialMeasurement - 273.15
+            "Fahrenheit"    -> 1.8 * (initialMeasurement - 273.15) + 32
+            "Kelvin"        -> initialMeasurement
+            else            -> 0.0
+        }
+        else                -> 0.0
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
+​
+​
+​
+​
+​
+​
+​
+​
+​
